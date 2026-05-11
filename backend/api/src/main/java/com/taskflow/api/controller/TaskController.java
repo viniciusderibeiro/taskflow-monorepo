798eeeea226d9ee1bcaae7ef29c7b1f6b4c9ce1e
Nyssim <viniciusjunioribeiro05@gmail.com>
2026-05-10 22:27:04 -0300

    chore: setup database, environment variables, and security classes

--- backend/api/src/main/java/com/taskflow/api/controller/TaskController.java
+++ backend/api/src/main/java/com/taskflow/api/controller/TaskController.java
@@ -2,9 +2,9 @@ package com.taskflow.api.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,41 +13,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taskflow.api.model.Task;
+import com.taskflow.api.model.enums.TaskPriority;
+import com.taskflow.api.model.enums.TaskStatus;
 import com.taskflow.api.service.TaskService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/task")
 @RequiredArgsConstructor
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
 
     @GetMapping
-    public ResponseEntity<List<Task>> findAll() {
-        return ResponseEntity.ok(taskService.findAll());
+    public List<Task> findAll() {
+        return taskService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.findById(id));
+    public Task findById(@PathVariable Long id) {
+        return taskService.findById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Task> save(@RequestBody Task task) {
-        return ResponseEntity.ok(taskService.save(task));
+    public Task save(@RequestBody Task task) {
+        return taskService.save(task);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody Task taskData) {
-        return ResponseEntity.ok(taskService.update(id, taskData));
+    public Task update(@PathVariable Long id, @RequestBody Task task) {
+        return taskService.update(id, task);
+    }
+
+    @PatchMapping("/{id}/priority")
+    public Task updatePriority(@PathVariable Long id, @RequestBody TaskPriority taskPriority) {
+        return taskService.updatePriority(id, taskPriority);
+    }
+
+    @PatchMapping("/{id}/status")
+    public Task updateStatus(@PathVariable Long id, @RequestBody TaskStatus taskStatus) {
+        return taskService.updateStatus(id, taskStatus);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    public void delete(@PathVariable Long id) {
         taskService.delete(id);
-        return ResponseEntity.noContent().build();
     }
-
 }
