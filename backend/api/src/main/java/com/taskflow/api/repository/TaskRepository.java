3ecf112faa37a1326b55360aebe50c9575cf60c0
Nyssim <viniciusjunioribeiro05@gmail.com>
2026-05-14 18:31:02 -0300

    feat: only taks for user

--- backend/api/src/main/java/com/taskflow/api/repository/TaskRepository.java
+++ backend/api/src/main/java/com/taskflow/api/repository/TaskRepository.java
@@ -1,11 +1,14 @@
 package com.taskflow.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.taskflow.api.model.Task;
+import com.taskflow.api.model.User;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    List<Task> findByUser(User user);
 }
